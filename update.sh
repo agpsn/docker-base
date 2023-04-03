8fb394f4ab923f6abc3e149a8759631712c3cdee
AGPSN <no-reply@agpsn.com>
2023-04-03 10:32:17 +0000

    Updated

--- update.sh
+++ update.sh
@@ -4,6 +4,6 @@ clear; set -eu
 echo $(cat .ghcr-token) | docker login ghcr.io -u $(cat .ghcr-user) --password-stdin &>/dev/null
 	GBRANCH=$(git branch | grep "*" | rev | cut -f1 -d" " | rev)
 	echo "Updating Base [$GBRANCH]"
-	docker build --force-rm --rm --tag ghcr.io/agpsn/docker-base:alpine .
+	docker build --quiet --force-rm --rm --tag ghcr.io/agpsn/docker-base:alpine .
 	git add . && git commit -am "Updated" && git push --quiet
 	docker push ghcr.io/agpsn/docker-base:alpine
