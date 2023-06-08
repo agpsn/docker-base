f1c9756d6394b002bd10ac8412b2e33974489493
AGPSN <github@agpsn.com>
2023-06-07 21:59:15 -0500

    Updated

--- update.sh
+++ update.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 set -eu
 
-[ ! -d "/mnt/user/system/agpsn-github/docker-base" ] && echo "No repo!" && exit 1
-cd "/mnt/user/system/agpsn-github/docker-base"
-
-echo $(cat ~/.ghcr-token) | docker login ghcr.io -u $(cat ~/.ghcr-user) --password-stdin &>/dev/null
+echo $(cat ../.token) | docker login ghcr.io -u $(cat ../.user) --password-stdin &>/dev/null
 
 echo "Updating Base"
-docker build --quiet --force-rm --rm --tag ghcr.io/agpsn/docker-base:alpine .
-git add . && git commit -am "Updated" && git push --quiet
+
+#docker build --quiet --force-rm --rm --tag ghcr.io/agpsn/docker-base:alpine .
+docker build --force-rm --rm --tag ghcr.io/agpsn/docker-base:alpine .
+
+#git add . && git commit -am "Updated" && git push --quiet
+git add . && git commit -am "Updated" && git push
+
 docker push --quiet ghcr.io/agpsn/docker-base:alpine
+docker push ghcr.io/agpsn/docker-base:alpine
+
 echo ""
