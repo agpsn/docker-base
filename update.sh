ce0b01fbac4f715e29e5a4da8850da32d4a2ea07
AGPSN <101997295+AGPSN-BOT@users.noreply.github.com>
2023-10-12 06:17:14 -0700

    Updated

--- update.sh
+++ update.sh
@@ -7,7 +7,7 @@ AVERSION=$(docker run --rm alpine:latest cat /etc/os-release | grep VERSION_ID |
 UVERSION=$(docker run --rm ubuntu:latest cat /etc/os-release | grep VERSION_ID | cut -f2 -d"=" | cut -f2 -d"\"")
 
 echo "Updating Alpine Base: v$AVERSION"
-docker build --force-rm --rm --tag ghcr.io/agpsn/alpine-base:latest --tag ghcr.io/agpsn/alpine-base:$AVERSION --tag ghcr.io/agpsn/alpine-base:alpine-latest -f ./Dockerfile.alpine .
+docker build --quiet --force-rm --rm --tag ghcr.io/agpsn/alpine-base:latest --tag ghcr.io/agpsn/alpine-base:$AVERSION --tag ghcr.io/agpsn/alpine-base:alpine-latest -f ./Dockerfile.alpine .
 git tag -f $AVERSION && git push --quiet origin $AVERSION -f --tags
 #git add . && git commit -am "Updated" && git push --quiet
 docker push --quiet ghcr.io/agpsn/alpine-base:latest
